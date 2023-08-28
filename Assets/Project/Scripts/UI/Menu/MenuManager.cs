577b771aeb3dc1fe5c5c38d7ef885918c541829d
zstardustttt <blastik2281337@gmail.com>
2023-08-28 17:42:03 +0700

    Console warnings fix

--- Assets/Project/Scripts/UI/Menu/MenuManager.cs
+++ Assets/Project/Scripts/UI/Menu/MenuManager.cs
@@ -1,13 +1,33 @@
 using MonoWaves.QoL;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private SceneField _startingScene;
 
+    [SerializeField] private EventSystem _eventSystem;
+    [SerializeField] private GameObject _camera;
+
+    private bool _listenerAdded;
+
+    private void Start()
+    {
+        _eventSystem.gameObject.SetActive(true);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(_startingScene.SceneName);
+    }
+
+    private void Update() 
+    {
+        if (!_listenerAdded)
+        {
+            _camera.AddComponent<AudioListener>();
+            _listenerAdded = true;
+        }
     }
 }
